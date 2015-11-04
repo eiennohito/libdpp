e222e97eec87f32e3d198f26b7c81956c43dd36d
Arseny Tolmachev <eiennohito@gmail.com>
2015-11-04 14:22:18 +0900

    fix interface
    v0.1.1

--- java/src/main/java/ws/eiennohito/jdpp/VectorFiller.java
+++ java/src/main/java/ws/eiennohito/jdpp/VectorFiller.java
@@ -4,7 +4,7 @@ package ws.eiennohito.jdpp;
  * @author eiennohito
  * @since 2015/11/03
  */
-interface VectorFiller {
+public interface VectorFiller {
   int fillDouble(double[] data, int start, int length);
   int fillFloat(float[] data, int start, int length);
   int sample(long[] result);
